14aa714a8d01fcaa6b13df9e7525c681fe0d591c
st0rm348 <brunommsoares@outlook.com>
2024-09-21 14:13:48 +0100

    feat(home): add phone component

---
+++ src/components/Phone.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+
+type Props = {
+  imgSrc: string,
+  className: string,
+  dark?: boolean
+}
+
+function Phone({ imgSrc, className, dark = false }: Props) {
+  return (
+    <div className={cn('relative z-50 overflow-hidden pointer-events-none select-none', className)}>
+      <Image
+        src={
+          dark
+            ? '/phone-template-dark-edges.png'
+            : '/phone-template-white-edges.png'
+        }
+        alt="phone image"
+        width={896}
+        height={1831}
+      />
+      <div className="absolute inset-0 -z-10">
+        <div className="relative object-cover min-w-full min-h-full">
+          <Image
+            src={imgSrc}
+            alt="overlaying phone image"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Phone;
