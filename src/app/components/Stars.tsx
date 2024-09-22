7d119ae31fd81189c879c40c9f695b74a2d9cf65
st0rm348 <brunommsoares@outlook.com>
2024-09-22 22:41:34 +0100

    refactor: make size as prop on Stars component

--- src/app/components/Stars.tsx
+++ src/app/components/Stars.tsx
@@ -3,17 +3,23 @@ import { Star } from 'lucide-react';
 
 type StarsProps = {
   quantity: number,
+  size: number,
   containerClassName?: string,
-  starClassName?: string
+  starClassName?: string,
 }
 
-function Stars({ quantity, containerClassName, starClassName }: StarsProps) {
+function Stars({
+  quantity,
+  size,
+  containerClassName,
+  starClassName,
+}: StarsProps) {
   return (
     <div className={cn('flex gap-0.5', containerClassName)}>
       {[...Array(quantity)].map(() => (
         <Star
           key={`stars-${getRandomInt(Number.POSITIVE_INFINITY)}`}
-          className={cn('h-4 w-4 text-green-600 fill-green-600', starClassName)}
+          className={cn(`h-${size} w-${size} text-green-600 fill-green-600`, starClassName)}
         />
       ))}
     </div>
