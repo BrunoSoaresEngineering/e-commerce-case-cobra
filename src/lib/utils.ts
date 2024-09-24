906b7a530815b46b43e0d93851425c5210532228
st0rm348 <brunommsoares@outlook.com>
2024-09-24 22:54:47 +0100

    feat(home): add utility functions
    - function to split an array in a certain number of sub-arrays
    - function to get number of columns on a CSS grid rendered in the DOM

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,33 @@ export function cn(...inputs: ClassValue[]) {
 export function getRandomInt(max: number): number {
   return Math.floor(Math.random() * max);
 }
+
+export function splitArray<T>(array: Array<T>, numParts: number) { 
+  if (numParts === 0) {
+    return [];
+  }
+
+  const result: Array<Array<T>> = [];
+
+  array.forEach((element, index) => {
+    const selectedArrayIndex = index % numParts;
+
+    if (!result[selectedArrayIndex]){
+      result[selectedArrayIndex] = [];
+    }
+
+    result[selectedArrayIndex].push(element);
+  });
+
+  return result;
+}
+
+export function getNumberOfGridColumns(grid: HTMLDivElement | null) {
+  if (!grid) {
+    return 0;
+  }
+
+  const gridComputedStyle = window.getComputedStyle(grid);
+
+  return gridComputedStyle.getPropertyValue('grid-template-columns').split(' ').length;
+}
