619a51f76f867d7bdf4206db620c1246fa507b2e
st0rm348 <brunommsoares@outlook.com>
2024-09-27 22:07:52 +0100

    feat(home): add footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import './globals.css';
-import Navbar from '../components/Navbar';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link';
+import MaxWidthWrapper from './Max-width-wrapper';
+
+function Footer() {
+  return (
+    <footer>
+      <MaxWidthWrapper>
+        <div
+          className="h-20 bg-white flex flex-col md:flex-row justify-center md:justify-between items-center gap-2 border-t border-gray-200"
+        >
+          <p className="text-sm text-center text-muted-foreground">
+            &copy;
+            {' '}
+            {new Date().getFullYear()}
+            {' '}
+            All rights reserved
+          </p>
+
+          <div className="text-sm text-muted-foreground space-x-8">
+            <Link href="#">Terms</Link>
+            <Link href="#">Privacy Policy</Link>
+            <Link href="#">Cookie Policy</Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </footer>
+  );
+}
+export default Footer;
