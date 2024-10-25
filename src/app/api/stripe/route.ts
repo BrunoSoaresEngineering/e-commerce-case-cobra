a3051c8b3b255688d1ff886c978e2dcaedd86582
st0rm348 <brunommsoares@outlook.com>
2024-10-26 00:46:00 +0100

    fix: emails sent from onboard resend email

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -75,7 +75,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: 'CaseCobra <brunommsoares@outlook.com>',
+        from: 'CaseCobra <onboarding@resend.dev>',
         to: [event.data.object.customer_details.email],
         subject: 'Congratulations on your new phone case!',
         react: OrderReceived({
