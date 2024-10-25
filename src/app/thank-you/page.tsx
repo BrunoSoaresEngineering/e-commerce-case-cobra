004585e4f636a373151f2e72ffe7fa23f7c9fe81
st0rm348 <brunommsoares@outlook.com>
2024-10-25 23:45:46 +0100

    fix(thankyou): missing Suspense boundary with useSearchParams
    https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import Providers from '@/components/Providers';
 import ThankYou from './_components/Thank-you';
 
 function ThankYouPage() {
   return (
     <Providers>
-      <ThankYou />
+      <Suspense>
+        <ThankYou />
+      </Suspense>
     </Providers>
   );
 }
